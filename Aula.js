ac3e83f665207555bf984f230e25483af2055260
frosttlink <leolorena.alves@gmail.com>
2024-08-02 11:39:38 -0300

    debseleos

--- Aula.js
+++ Aula.js
@@ -12,4 +12,17 @@ servidor.get ( '/calculadora/:n1/:n2' , ( req, resp ) => {
     } )
 });
 
+servidor.get ( '/calculadora/subtracao/:n1/:n2' , ( req, resp ) => {
+    let n1 = Number ( req.params.n1 )
+    let n2 = Number ( req.params.n2 )
+
+    let subtrair = n1 + n2
+
+    resp.send( {
+        primeiroAlgarismo: n1,
+        segundoAlgarismo: n2,
+        resultadoDaSubtracao: subtrair
+    } )
+});
+
 servidor.listen(5050, () => console.log("Subiu pae"))
